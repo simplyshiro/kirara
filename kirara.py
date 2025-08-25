c200382ecc6ca1bf8134e26b148fbf5314cda741
shiro <45232262+simplyshiro@users.noreply.github.com>
2025-08-25 13:59:25 +0800

    refactor: simplify handling of cookies
    Remove `set_cookies` function and pass the `COOKIES` environment variable as a
    string to `genshin.Client.set_cookies()`.

--- kirara.py
+++ kirara.py
@@ -8,24 +8,11 @@ import genshin
 
 
 async def main():
-    env_cookies = os.getenv("COOKIES")
+    cookies = os.getenv("COOKIES")
 
-    if not env_cookies:
+    if not cookies:
         print("Environment variable COOKIES is not set.")
         sys.exit(1)
-
-    try:
-        cookies = http.cookies.SimpleCookie(env_cookies)
-    except http.cookies.CookieError:
-        print("Error parsing cookies.")
-        sys.exit(1)
-
-    required_cookies = ["account_id_v2", "cookie_token_v2"]
-
-    for cookie in required_cookies:
-        if cookie not in cookies:
-            print(f"Missing {cookie} in the COOKIES environment variable.")
-            sys.exit(1)
 
     env_games = os.getenv("GAMES")
     games_to_claim_daily_reward = []
@@ -48,25 +35,16 @@ async def main():
             print(f"{game} is not a valid game. Skipping.")
 
 
-async def claim_daily_reward(game: genshin.Game, cookies: http.cookies.SimpleCookie):
-    client = genshin.Client(game=game)
-
-    await set_cookies(client, cookies)
+async def claim_daily_reward(game: genshin.Game, cookies: str):
+    client = genshin.Client(game=game, cookies=cookies)
 
     try:
         await client.claim_daily_reward(reward=False)
     except genshin.AlreadyClaimed:
         print(f"Already claimed the daily reward for {game.name} today.")
     except genshin.InvalidCookies:
-        print("Either account_id_v2 or cookie_token_v2 is invalid.")
+        print("Invalid cookies detected.")
         sys.exit(1)
-
-
-async def set_cookies(client: genshin.Client, cookies: http.cookies.SimpleCookie):
-    account_id_v2 = cookies["account_id_v2"].value
-    cookie_token_v2 = cookies["cookie_token_v2"].value
-
-    client.set_cookies(account_id_v2=account_id_v2, cookie_token_v2=cookie_token_v2)
 
 
 if __name__ == "__main__":
